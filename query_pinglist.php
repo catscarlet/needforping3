82e2261903a56d79072a8a370d1464f3d37f4f63
catscarlet <i@catscarlet.com>
2015-07-31 22:49:28 +0800

    删除无用代码，增加get请求pingresult

--- query_pinglist.php
+++ query_pinglist.php
@@ -33,13 +33,11 @@ class ServerList
 
     public function echoCheckbox()
     {
-      echo '
-      <input type="checkbox" name="selecter" value='.$this->server_name.')">'.$this->alias_name.'
+        echo '
+      <input type="checkbox" name="selected_server" value='.$this->server_name.'>'.$this->alias_name.'
       <br>
       ';
     }
-
-
 }
 $serverarray = array();
 //$serverlist = new ServerList();
@@ -51,47 +49,26 @@ if (!$con) {
     die('Could not connect: '.mysql_error());
 }
 
-/* 查询数据库，输出对象，而非数组 */
+/* 查询数据库 */
 mysql_select_db(constant('DB_NAME'), $con) or die(mysql_error());
 $sql = 'select * from '.constant('DB_TABLE').' where state = "normal" order by id';
-//var_dump($sql);
 $result = mysql_query($sql) or die('Query failed: '.mysql_error().' Actual query: '.$sql);
-//var_dump($result);
+
+
+/* html输出form */
+echo '<form class="formcss" action="query_db.php" method="get">';
 
 while ($row = mysql_fetch_array($result)) {
-
-
-$serverarray[$row['id']] = new ServerList($row['id'], $row['server_name'], $row['alias_name'], $row['description'], $row['state']);
-//$serverarray = new ServerList(1,2,3,4,5);
-//var_dump($row['id'], $row['server_name'], $row['alias_name'], $row['description'], $row['state']);;
-//echo $serverarray[$row['id']]->echoMySelf();
-$serverarray[$row['id']]->echoCheckbox();
+    $serverarray[$row['id']] = new ServerList($row['id'], $row['server_name'], $row['alias_name'], $row['description'], $row['state']);
+    $serverarray[$row['id']]->echoCheckbox();
 }
 
-/*
+echo '<input type="submit" name="submit" value="提交">
 
-$i = min($query_range, mysql_num_rows($result));
-while ($row = mysql_fetch_array($result) and $i >= 0) {
-    $i = $i - 1;
-    $query_DATA['DATETIME'][$i] = $row['DATETIME'];
-    $query_DATA['loss_percent'][$i] = 100 - substr($row['loss_percent'], 0, -1);
-    $query_DATA['rtt_avg'][$i] = round($row['rtt_avg']);
-}
 
- // 因为sql查询是DESC的，所以要根据键值重新排序，不然坐标轴的时间会变成降序
-ksort($query_DATA['DATETIME']);
-ksort($query_DATA['loss_percent']);
-ksort($query_DATA['rtt_avg']);
+</form>';
 
- // 将查询的关键词与查询结果合并，并输出json
 
-$query_data = array('server_name' => $q);
-$query_data = array_merge($query_data, $query_DATA);
-echo json_encode($query_data);
-
-mysql_close($con);
-
-*/
 
 function pingresult_clean($q)
 {
